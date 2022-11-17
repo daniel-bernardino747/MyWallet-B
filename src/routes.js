027bf10d22c87ccc0cb90884398c81667da9e802
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-17 16:50:11 -0300

    feat: create rout sign-in

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { registerClient } from './controllers/auth.controller.js';
+import { registerClient, signIn } from './controllers/auth.controller.js';
 
 const routes = express.Router();
 
 routes.post('/sign-up', registerClient);
+
+routes.post('/sign-in', signIn);
 
 export default routes;
