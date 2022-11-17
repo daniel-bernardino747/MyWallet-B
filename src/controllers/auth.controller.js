2b949935bed921488213a4314066db5670f0f337
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-17 16:46:59 -0300

    feat: create routes auth

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import { usersCollection, sessionsCollection } from '../server.js';
+
+export async function registerClient(req, res) {
+  const { name, email, password } = req.body;
+
+  const hashPassword = bcrypt.hashSync(password, 10);
+  try {
+    await usersCollection.insertOne({ name, email, password: hashPassword });
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+}
+
+export async function signIn(res, req) {
+  const { email, password } = req.body;
+  const token = uuid();
+
+  try {
+    const existingUser = await usersCollection.findOne({ email });
+    if (!existingUser) return res.sendStatus(401);
+
+    const correctPassword = bcrypt.compare(password, existingUser.password);
+    if (!correctPassword) return res.sendStatus(401);
+
+    await sessionsCollection.insertOne({ token, userId: existingUser._id });
+
+    return res.status(201).json({ token });
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+}
