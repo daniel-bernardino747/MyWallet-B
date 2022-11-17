01f7860316cd4691e2f8feb1bf224da013d91418
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-17 16:46:49 -0300

    feat: create route sign-up

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import { registerClient } from './controllers/auth.controller.js';
 
 const routes = express.Router();
+
+routes.post('/sign-up', registerClient);
 
 export default routes;
