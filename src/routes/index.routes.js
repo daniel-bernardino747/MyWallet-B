1b88901690a422781e6f9e46555ca5514d40b2d6
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-19 22:25:27 -0300

    refactor: create past routes and index

---
+++ src/routes/index.routes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { registerClient, signIn } from '../controllers/auth.controller.js';
+import { getAllTransaction, postTransaction } from '../controllers/transactions.controller.js';
+import authValidate from '../middlewares/auth.middleware.js';
+import { existingUserValidate, incorrectUserValidate } from '../middlewares/user.middleware.js';
+
+const routes = express.Router();
+
+// auth routes
+
+routes.post('/sign-up', existingUserValidate, registerClient);
+
+routes.post('/sign-in', incorrectUserValidate, signIn);
+
+// transactions routes
+
+routes.use(authValidate);
+
+routes.post('/transactions', postTransaction);
+
+routes.get('/transactions', getAllTransaction);
+
+export default routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes.js';
+import routes from './routes/index.routes.js';
 
 const app = express();
 const port = 5000;
