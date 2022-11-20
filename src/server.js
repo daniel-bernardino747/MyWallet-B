276569cb6de80ae9eaf8f9fefb06cd6ab0e1d00a
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-19 22:30:58 -0300

    refactor: save PORT in env

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors';
+
 import routes from './routes/index.routes.js';
 
-const app = express();
-const port = 5000;
+dotenv.config();
 
+const app = express();
 app.use(cors())
   .use(express.json())
   .use(routes);
 
+const port = process.env.PORT || 3333;
 app.listen(port, () => {
   console.log(`🌀 started server in door: ${port}`);
 });
